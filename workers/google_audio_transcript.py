855bc5c00ef7b046caacab76a234516173f9ef5d
Qihong Shao <tiffany.asu@gmail.com>
2018-05-02 16:28:12 -0700

    finish google audio worker

--- workers/google_audio_transcript.py
+++ workers/google_audio_transcript.py
@@ -14,7 +14,7 @@ _client = pymongo.MongoClient('mongodb://localhost:4000/')
 _demodb = _client['demodb']
 _tasks = _demodb.tasks
 
-_timeout = 900
+_timeout = 3600
 
 _storage = google.cloud.storage.Client()
 _speech = google.cloud.speech.SpeechClient()
@@ -68,14 +68,9 @@ def GoogleSpeechTranslate(uri):
 
 
 def OutputReadableFile(response):
-    total_item = len(response["results"])
-    keys = []
-    for i in range(0,total_item):
-        keys.append(data["results"][i]["alternatives"][0]["transcript"].strip().strip('"'))
     l = []
-    for key in keys:
-        l.append(key)
-        l.append("\n")
+    for result in response.results:
+        l.append(result.alternatives[0].transcript)
     content = ''.join(l)
     return content
 
@@ -86,17 +81,17 @@ def Process(task):
     mp4file = task['videofile']
     if not basedir or not mp4file:
         task['google_audio_status'] = -1
-        err = '## Error: basedir or videofile cannot be empty'
-        return err
+        print('## Error: basedir or videofile cannot be empty')
+        return
     user = task['user']
     if not user:
         task['google_audio_status'] = -1
-        err = '## Error: user cannot be empty'
-        return err
+        print('## Error: user cannot be empty')
+        return 
     err, gsurl = UploadToGoogleStorage(basedir, mp4file, user)
     if err:
-        return err
-    #gsurl = 'gs://speech2text--storage/sample.flac'
+        print(err)
+        return 
     gsurl = gsurl.replace('https://storage.googleapis.com/', 'gs://', 1)
     print('Calling translate API with:', gsurl)
     response = GoogleSpeechTranslate(gsurl)
@@ -104,60 +99,31 @@ def Process(task):
     output_content = OutputReadableFile(response)
     outputfile = os.path.join(basedir, 'googleaudiofile.json')
     with open(outputfile, 'w') as f:
-        f.write(content)
-
-"""
-results {
-  alternatives {
-    transcript: "what was the Maryland Lottery number"
-    confidence: 0.8972623944282532
-  }
-}
-results {
-  alternatives {
-    transcript: "penicillin 190"
-    confidence: 0.8290839195251465
-  }
-}
-results {
-  alternatives {
-    transcript: " 190"
-    confidence: 0.6241641044616699
-  }
-}
-"""
-    # if err:
-    #     task['audio_status'] = -1
-    #     print('## Error: Translate failed')
-    #     return
-   
-    # output_file = OutputReadableFile(transfile)
-    # # Copy to download place.
-    # soln_id = os.path.basename(basedir)
-    # destdir = os.path.join('/home/cisco/qishao/WebExDemo/public/'
-    #                        'soln', soln_id)
-    # os.makedirs(destdir, exist_ok=True)
-    # dest_file = os.path.join(destdir, 'google_audio_output.txt')
-    # cmd = ' '.join(['cp -f', output_file, dest_file])
-    # print('Copy to download place:', cmd)
-    # subprocess.call(cmd, shell=True)
-    # task['audio_status'] = 1
-    # task['audio_output'] = os.path.join('/soln', soln_id, 'google_audio_output.txt'
-    # )
+        f.write(output_content)
+    # Copy to download place.
+    soln_id = os.path.basename(basedir)
+    destdir = os.path.join('/home/cisco/qishao/WebExDemo/public/'
+                           'soln', soln_id)
+    os.makedirs(destdir, exist_ok=True)
+    destfile = os.path.join(destdir, 'google_audio_output.txt')
+    cmd = ' '.join(['cp -f', outputfile, destfile])
+    print('Copy to download place:', cmd)
+    subprocess.call(cmd, shell=True)
+    task['google_audio_status'] = 1
+    task['google_audio_output'] = os.path.join('/soln', soln_id, 'google_audio_output.txt')
 
 
 if __name__ == "__main__":
     while True:
         for task in _tasks.find({'google_audio_status': 0}):
-            err = Process(task)
-            print('ERROR:', err)
-            # print('updating',task, '...\n')
-            # print(_tasks.update_one(
-            #     {'_id': task['_id']},
-            #     { '$set': {
-            #         'audio_status': task['audio_status'],
-            #         'audio_output': task['audio_output']
-            #     }}))
+            Process(task)
+            print('updating',task, '...\n')
+            print(_tasks.update_one(
+                {'_id': task['_id']},
+                { '$set': {
+                    'google_audio_status': task['google_audio_status'],
+                    'google_audio_output': task['google_audio_output']
+                }}))
         print('No more task, sleep for 5s')
         break
         time.sleep(5)
